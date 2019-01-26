9cdbc44c1c63c90b4fa533f67f21d3c30f2aee68
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-26 23:15:23 +0530

    Asynchronous function and Promise

--- V05-promiseAndAsyncAwait.js
+++ V05-promiseAndAsyncAwait.js
@@ -1,13 +1,22 @@
 function one(){
     console.log(`I am 1`);
 }
-// function two(){
-//     console.log(`I am 2`);
-// }
 function two(){
+    console.log(`I am 2`);
+}
+function twoC(){
     setTimeout(function(){
-        console.log(`I am 2`);
+        console.log(`I am 2C`);
     }, 4000);    
+}
+function twoP(){
+    return new Promise(function (resolve){
+        setTimeout(function(){
+            console.log(`I am 2P`);
+            resolve("Done!!")
+        }, 4000); 
+    });
+       
 }
 function three(){
     console.log(`I am 3`);
@@ -15,6 +24,9 @@ function three(){
 function four(){
     one();
     two();
+    twoC();
+    twoP();
     three();
 }
 four();
+console.log(`==========================================================`);
