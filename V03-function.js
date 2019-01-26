1e88fe6a4e6f7ef2416aaec6da9a81a1fda06166
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-26 19:34:48 +0530

    functions

---
+++ V03-function.js
@@ -0,0 +1,10 @@
+function add1(a1, a2){
+    return a1 + a2;
+}
+console.log(add1(2,3));
+
+var add2 = function (a1, a2){
+    return a1 + a2;
+}
+console.log(add2(2,3));
+//-----------------------------
