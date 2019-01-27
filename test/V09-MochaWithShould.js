518e3effed44b779f112b9f7bc58be1f2caf09fd
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-27 18:37:44 +0530

    Mocha with should

--- test/V09-MochaWithShould.js
+++ test/V09-MochaWithShould.js
@@ -6,18 +6,18 @@ describe('1.test Suite', function(){
         true.should.equal(true);
     })
     it('2nd test case', function(){
-        false.should.equal(true);
+        false.should.equal(true);//AssertionError: expected false to be true
     })
     it('3rd test case', function(){
         ('Krishna').should.equal('Krishna');
     })
     it('4th test case', function(){
-        ('Krishna').should.equal('Rama');
+        ('Krishna').should.equal('Rama');//AssertionError: expected 'Krishna' to be 'Rama'
     })
     it('5th test case', function(){
         ('HareKrishna').should.containEql('Krishna');
     })
     it('6th test case', function(){
-        ('HareKrishna').should.containEql('Rama');
+        ('HareKrishna').should.containEql('Rama'); //AssertionError: expected 'HareKrishna' to contain 'Rama'
     })
 })
