38507445dae14c2c70919799049931e7bf04cbfd
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-27 16:39:04 +0530

    Mocha first test

---
+++ test/V07-mochaTest.js
@@ -0,0 +1,26 @@
+const mocha = require('mocha');
+
+
+describe('Test first Suite',function(){
+    it('My First test case', function(){
+        console.log("1. first test case");
+    })
+    it('My Second test case', function(){
+        console.log("2. Second test case");
+    })
+    it('My Third test case', function(){
+        console.log("3. Third test case");
+    })
+});
+
+describe('Test second Suite',function(){
+    it('My First test case', function(){
+        console.log("1. first test case");
+    })
+    it('My Second test case', function(){
+        console.log("2. Second test case");
+    })
+    it('My Third test case', function(){
+        console.log("3. Third test case");
+    })
+});
