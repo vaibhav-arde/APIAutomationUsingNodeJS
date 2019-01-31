d9e8c849cdcbe77425c27ad2b66f983b1e37dcf0
vaibhav-arde <vaibhav.arde@gmail.com>
2019-01-31 23:25:15 +0530

    delete, status

--- test/V14-Delete.js
+++ test/V14-Delete.js
@@ -8,53 +8,57 @@ let createUserEndPoint = 'api/users';
 let updateUserEndPoint = 'api/users/2';
 let deleteUserEndPoint = 'api/users/2';
 
-it.only('Delete User', async function(){
-    let res = await baseUrl.delete(deleteUserEndPoint)
-        .set('Authorisation', '123456')
-        .set('Accept', 'application/json')
-    res.status.should.equal(204);
-})
-it('Update User', async function(){
-    let res = await baseUrl.put(updateUserEndPoint)
-        .set('Authorization', '123456')
-        .set('Accept', 'application/json')
-        .send({
-            "Name":"Ram",
-            "Responsible" : "The King"
-          })
-    console.log(res.body);
-    res.status.should.equal(200);
-    res.body.Name.should.be.a.String();
-    res.body.Responsible.should.be.a.String();
-    res.body.updatedAt.should.be.a.String();
+describe('Demo test Suite', function(){
+    it('Delete User', async function(){
+        let res = await baseUrl.delete(deleteUserEndPoint)
+            .set('Authorisation', '123456')
+            .set('Accept', 'application/json')
+        res.status.should.equal(204);
+    })
+    
+    it('Update User', async function(){
+        let res = await baseUrl.put(updateUserEndPoint)
+            .set('Authorization', '123456')
+            .set('Accept', 'application/json')
+            .send({
+                "Name":"Ram",
+                "Responsible" : "The King"
+              })
+        console.log(res.body);
+        res.status.should.equal(200);
+        res.body.Name.should.be.a.String();
+        res.body.Responsible.should.be.a.String();
+        res.body.updatedAt.should.be.a.String();
+    })
+    
+    it('Create User', async function(){
+        let res = await baseUrl.post(createUserEndPoint)
+            .send({
+                "Name":"Ram",
+                "Responsible" : "King"
+            });
+        console.log(res.body);
+        res.body.Name.should.be.a.String();
+        res.body.Responsible.should.be.a.String();
+        res.body.id.should.be.a.String();
+        res.body.createdAt.should.be.a.String();
+    })
+    
+    it('List User', async function(){
+        let res = await baseUrl.get(listUserEndPoint);
+        console.log(res.body);
+        res.body.page.should.be.a.Number();
+        res.body.per_page.should.be.a.Number();
+        res.body.total.should.be.a.Number();
+        res.body.total_pages.should.be.a.Number();
+        res.body.data.should.be.a.Object();
+        //console.log(res.body.data.length);
+        for(let i=0; i<res.body.data.length; i++){
+            res.body.data[i].id.should.be.a.Number();
+            res.body.data[i].first_name.should.be.a.String();
+            res.body.data[i].last_name.should.be.a.String();
+            res.body.data[i].avatar.should.be.a.String();
+        }
+    });
 })
 
-it('Create User', async function(){
-    let res = await baseUrl.post(createUserEndPoint)
-        .send({
-            "Name":"Ram",
-            "Responsible" : "King"
-        });
-    console.log(res.body);
-    res.body.Name.should.be.a.String();
-    res.body.Responsible.should.be.a.String();
-    res.body.id.should.be.a.String();
-    res.body.createdAt.should.be.a.String();
-})
-
-it('List User', async function(){
-    let res = await baseUrl.get(listUserEndPoint);
-    console.log(res.body);
-    res.body.page.should.be.a.Number();
-    res.body.per_page.should.be.a.Number();
-    res.body.total.should.be.a.Number();
-    res.body.total_pages.should.be.a.Number();
-    res.body.data.should.be.a.Object();
-    //console.log(res.body.data.length);
-    for(let i=0; i<res.body.data.length; i++){
-        res.body.data[i].id.should.be.a.Number();
-        res.body.data[i].first_name.should.be.a.String();
-        res.body.data[i].last_name.should.be.a.String();
-        res.body.data[i].avatar.should.be.a.String();
-    }
-});
